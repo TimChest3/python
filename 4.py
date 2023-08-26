32e621fda4479f70a2d47633314012b4e3b090b3
TimChest3 <116972199+TimChest3@users.noreply.github.com>
2023-08-26 18:03:13 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,5 @@
+a = input("Enter the animal type: ")
+b = input("Enter pet name: ")
+c = input("Enter the age of the pet: ")
+
+print("It's a", a, "named", b, "Age:", c)
---
+++ 6.py
@@ -0,0 +1,7 @@
+a1 = input("Stage 1? ")
+a2 = input("Stage 2? ")
+a3 = input("Stage 3? ")
+a4 = input("Stage 4? ")
+a5 = input("Stage 5? ")
+a6 = input("Stage 6? ")
+print(a1, a2, a3, a4, a5, a6, sep=">")
