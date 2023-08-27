62a68b2cc5d4882005e43ef5205b1eaf96610fed
TimChest3 <116972199+TimChest3@users.noreply.github.com>
2023-08-27 11:46:27 +0300

    Add files via upload

---
+++ 9.2222.py
@@ -0,0 +1,4 @@
+a = float(input("Vvedite 1 storony: "))
+b = float(input("Vvedite 2 storony: "))
+print("Ploshchad'= ", a * b)
+print("Perimetr'= ", (a + b) * 2)
---
+++ 9.3333.py
@@ -0,0 +1,3 @@
+l = input("Vvedite pyatiznachnoe chislo: ")
+a, t, s , d, e = map(int, l)
+print(d ** e * s / (a - t))
